2015589b59aa75cbe5b5a25ed9dd1f81486c0d19
MorrganScott <123009753+MorrganScott@users.noreply.github.com>
2024-11-25 13:17:31 -0500

    Added facedetecthaar code to file

--- objectoriented.py
+++ objectoriented.py
@@ -1,10 +1,29 @@
 #This will be an object oriented version
 #of the virtual 3d game
+import cv2
+import numpy
 class FaceFinder:
   """use harcaasecade filter to detect largest face from a frame"""
   def __init__(self):
     print('Face Finder intitialize')
+    self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
+  def find_face(self,frame):
+    """returns face center (x,y), draws a rect on the frame"""
+    #convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = self.face_cascade.detectMultiScale(gray)
+    #Draw rectangle
+    if faces is None:
+      return None
+    bx=by=bw=bh=0
+    for (x, y, w, h) in faces:
+      if w>bw:
+        bx,by,bw.bh=x,y,w,h
+    cv2.rectangle(frame, (bx,by), (bx+bw, by+bh), (0, 255, 255), 5)
+    return (bx+bw/2, by+bh/2)
+  
 #-------------------------------------------------------
 #main
 print('starting OO virtual3d')
