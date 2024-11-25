2e918384fad765829c913177c478d013b94a4ded
MorrganScott <123009753+MorrganScott@users.noreply.github.com>
2024-11-25 12:38:30 -0500

    Update objectoriented.py

--- objectoriented.py
+++ objectoriented.py
@@ -1,3 +1,12 @@
 #This will be an object oriented version
 #of the virtual 3d game
+class FaceFinder:
+  """use harcaasecade filter to detect largest face from a frame"""
+  def __init__(self):
+    print('Face Finder intitialize')
+
+#-------------------------------------------------------
+#main
 print('starting OO virtual3d')
+ff = FaceFinder()
+print('virtual3d Complete')
