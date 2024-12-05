73b15837d545382aee0463dddeb29d86d08c360b
MorrganScott <123009753+MorrganScott@users.noreply.github.com>
2024-12-05 13:49:18 -0500

    Added the rest of the main code to the file

--- objectoriented.py
+++ objectoriented.py
@@ -22,10 +22,10 @@ class FaceFinder:
     for (x, y, w, h) in faces:
       if w>bw:
         bx,by,bw,bh=x,y,w,h
-    cv2.rectangle(frame, (bx,by), (bx+bw, by+bh), (0, 255, 255), 5)
-    return (bx+bw/2, by+bh/2)
+    cv2.rectangle(frame, (bx,by), (bx+bw, by+bh), (0, 255, 255), 3)
+    return ((bx+bw//2), by + (bh//2))
 
-class Satge():
+class Stage():
   #initialized with display size. draws background grid based on position
   def __init___(self):
     self.disp_h = 0
@@ -34,21 +34,21 @@ class Satge():
     self.cam_w = 1280
     self.save_x = 960
 
-  def draw_target_xy(self, img, pos, size): #Draws the target
+  def draw_target_xy(self, img, pos, size):##Draws the target
     cv2.circle(img, pos, size, (0, 0, 255), -1)
     cv2.circle(img, pos, int(size*.8), (255, 255, 255), -1)
     cv2.circle(img, pos, int(size*.6), (0, 0, 255), -1)
     cv2.circle(img, pos, int(size*.4), (255, 255, 255), -1)
     cv2.circle(img, pos, int(size*.2), (0, 0, 255), -1)
 
-  def draw_targetz(self, pos, facexy):#Draws a target with its position and 
-    #size relative to the user's position
+  def draw_targetz(self, pos, facexy):##Draws a target with its position and 
+    ##size relative to the user's position
     tx, ty, tz = pos
     cv2.circle(img, (targ0x, targ0y), 50, (255, 255, 0), -1)
     cv2.circle(img, (960+int((600-960)*.3**2), 540), 
      (targ0x, targ0y), (255, 255, 0), -1)
     
-  def update(self, facexy): #Redraws the screen
+  def update(self, facexy): ##Redraws the screen
     x,y = facexy
     e = .9
     x = e * x + (1-e*self.save_x)
@@ -63,7 +63,7 @@ class Satge():
       dx = int(dx * decay)
       #print(sx,sy)
       cv2.rectangle(img, (sx+dx,sy),(1920-sx+dx, 1080-sy), 
-       (255, 255, 266), 1)#Draws targets with trainig line
+       (255, 255, 266), 1)##Draws targets with trainig line
 
       targ0x = 600 + int((x - self.cam_w/2)*2*.6)
       targ0y = 540
@@ -88,7 +88,7 @@ class Satge():
       self.draw_target_xy(img, (targ2x, targ2y), 50)
 
 
-    cv2.imshow("Morrgan's Game", img)#Displays the NDarray
+    cv2.imshow("Morrgan's Game", img)##Displays the NDarray
 
 
 
@@ -98,35 +98,36 @@ class Satge():
 #main
 print('starting OO virtual3d')
 ff = FaceFinder()
+stage = Stage()
+img = np.zeros([1080,1920,3])
+cv2.imshow("Morrgan's Game", img)
 #create cam/ FaceFinder Instance
 cap = cv2.VideoCapture(cv2.CAP_ANY)
 if not cap.isOpened():
   print("Couldn't open cam")
   exit()
 
-
-
-
+moved = False
 while True:
   #pass
-  retval, frame = cap.read()
-  if retval == False:
-    print("camera error!")
+  ret, frame = cap.read()
+  if not ret:
+    print("Error reading the frame. Exiting...")
 
-  ff.find_face(frame)
-  cv2.imshow('q to quit', frame)
-  
+  facexy = ff.find_face(frame)
+  frame_small = cv2.resize(frame, (frame.shape[1]//4, frame.shape[0]//4), 
+                           interpolation = cv2.INTER_LINEAR)
+  cv2.imshow('q to quit', frame_small)
+  if not moved:
+    cv2.moveWindow('q to quit', 1080, 0)
+    moved = True
+  if facexy is not None:
+    img = stage.update(facexy)
+
+  #stop if q key is pressed
   if cv2.waitKey(30) == ord('q'):
     break
 
-
-
-
-
-pause = input('press enter to end')
-
-#destroy cam
+#Release the VideoCapture object
 cap.release()
-
 cv2.destroyAllWindows()
-print('virtual3d Complete')
